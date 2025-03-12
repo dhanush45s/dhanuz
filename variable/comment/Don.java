72960f63a1b5e1c8d98c6bbbe0003dd7faae245d
dhanush45s <dhanushgopi640@gmail.com>
2025-03-12 14:15:32 +0530

    college

---
+++ variable/comment/Don.java
@@ -0,0 +1,8 @@
+public class Don{
+    public static void main(String[] args){
+        int num = 30; 
+        int num1 = 60;
+        System.out.println(num);
+        System.out.println(num1);
+    }
+}
