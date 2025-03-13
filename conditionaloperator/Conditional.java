57825455f8c9075afc7eae48bedd940f41cb9210
dhanush45s <dhanushgopi640@gmail.com>
2025-03-13 10:32:51 +0530

    program

---
+++ conditionaloperator/Conditional.java
@@ -0,0 +1,13 @@
+public class Conditional{
+    
+    public static void main(String[] args){
+        int a = 10;
+        int b = 2;
+        System.out.println(10<2);
+        System.out.println(10>2);
+        System.out.println(10<=2);
+        System.out.println(10>=2);
+        System.out.println(10==2);
+        System.out.println(10!=2);
+    }
+}
