f9fc5eda3bd9502d2926026bc4ceddfe7be8485d
dhanush45s <dhanushgopi640@gmail.com>
2025-03-13 11:46:03 +0530

    positive

---
+++ positivenegative/Negpos.java
@@ -0,0 +1,15 @@
+public class Negpos{
+    int a;
+    public static void main(String[] args){
+        int a = 0;
+        if(a>0){
+            System.out.println("a is positive");
+        }
+        else if(a==0){
+            System.out.println("a is neutral");
+        }
+        else{
+            System.out.println("a is negative");
+        }
+    }
+}
