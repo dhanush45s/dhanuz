4c24b172cb9515e1c3233421ca776495e4bc84eb
dhanush45s <dhanushgopi640@gmail.com>
2025-03-13 10:49:56 +0530

    state

---
+++ conditionalstatement/Statement.java
@@ -0,0 +1,12 @@
+public class Statement{
+    public static void main(String[] args){
+    if(true){
+        System.out.println(true);
+
+    }
+    if(false){
+        System.out.println(false);
+
+    }
+    }
+}
