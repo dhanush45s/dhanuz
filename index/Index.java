af5505ad47f882bd5f3645c42abeb29f6b17d1f0
dhanush45s <dhanushgopi640@gmail.com>
2025-03-14 12:14:17 +0530

    dream

---
+++ index/Index.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Index{
+    public static void main(String[] args){
+        String name = "hello,world";
+
+        System.out.println(name.indexOf("w"));
+
+    }
+}         
