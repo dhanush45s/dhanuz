bd36941ae11563bbfb8d342e3f389e8557d4dac0
dhanush45s <dhanushgopi640@gmail.com>
2025-03-14 14:04:52 +0530

    inheritance

---
+++ heiarchicalinheritance/Heiarchical.java
@@ -0,0 +1,26 @@
+class Parent{
+    public void add(){
+        System.out.println("hello world");
+
+    }
+}
+class Child extends Parent{
+    void display(){
+        add();
+    }
+}
+
+class Third extends Child{
+    void helper(){
+        display();
+    }
+}
+public class Heiarchical{
+    public static void main(String[] args){
+        Third obj = new Third();
+    obj.helper();
+    Child obj1= new Child();
+    obj1.display();
+    }
+    
+}
