7861ab2a584aafabf2471bdaa5025d829bdb3985
dhanush45s <dhanushgopi640@gmail.com>
2025-03-14 12:20:09 +0530

    answer

---
+++ add/Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Add{
+    void add(int a,int b){
+        System.out.println("answer is "+a+b);
+    }
+    void add(int a, int b, int c){
+        System.out.println("answer is "+a+b+c);
+    }
+    public static void main(String[] args){
+        Add obj=new Add();
+        obj.add(10,20);
+        obj.add(20,30,40);
+    }
+}
