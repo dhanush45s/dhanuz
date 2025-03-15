7d7c912b4f60d2e6a5b577c7b122fd22c6070e83
dhanush45s <dhanushgopi640@gmail.com>
2025-03-15 11:13:56 +0530

    gen

---
+++ genderfinder/Gender.java
@@ -0,0 +1,11 @@
+public class Gender{
+    public static void main(String[] args){
+        String a="male";
+        if(a=="male"){
+             System.out.println("this  is male.");
+}
+else{
+     System.out.println("this is female.");
+}
+}
+}
