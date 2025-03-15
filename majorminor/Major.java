7626c54da8e434f741887d09c5e2674115a3ffd2
dhanush45s <dhanushgopi640@gmail.com>
2025-03-15 11:06:02 +0530

    value

---
+++ majorminor/Major.java
@@ -0,0 +1,12 @@
+public class Major{
+    public static void main(String[] args){
+        int a=10;
+        if(a>18){
+            System.out.println("this is major");
+
+        }
+        else{
+            System.out.println("this is minor");
+        }
+    }
+}
