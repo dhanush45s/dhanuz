b11aedc014990f1ca6bdd6111ef3b69dd4ffd663
dhanush45s <dhanushgopi640@gmail.com>
2025-03-15 10:42:33 +0530

    cn

---
+++ constructor/Constructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Constructor{
+    Constructor(){
+        System.out.println("welcome");
+
+    }
+    public static void main(String[] args){
+        Constructor obj = new Constructor();
+    }
+}
